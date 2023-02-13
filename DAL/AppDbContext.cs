81e230c11efb034a9bdfa1a445e99f69ae993c8f
davidchavezgrant <david@lines.by>
2023-02-13 03:40:56 -0800

    config

--- DAL/AppDbContext.cs
+++ DAL/AppDbContext.cs
@@ -12,5 +12,8 @@ internal sealed class AppDbContext: DbContext
 
 internal sealed record User(Guid Id, string Username)
 {
+	/// <summary>
+	/// This parameterless constructor is needed for EF Core and the admin panel to work.
+	/// </summary>
 	public User(): this(Guid.Empty, string.Empty) {}
 }
