f70012bdb98194eb79d152210a3ec8b9ccc972c7
davidchavezgrant <david@lines.by>
2023-02-13 03:56:29 -0800

    hello

--- DAL/AppDbContext.cs
+++ DAL/AppDbContext.cs
@@ -5,15 +5,12 @@ namespace Hackathon.DAL;
 
 internal sealed class AppDbContext: DbContext
 {
-	public AppDbContext(DbContextOptions<AppDbContext> options): base(options) {}
+	public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { this.Database.EnsureCreated(); }
 
 	public DbSet<User> Users => Set<User>();
 }
 
 internal sealed record User(Guid Id, string Username)
 {
-	/// <summary>
-	/// This parameterless constructor is needed for EF Core and the admin panel to work.
-	/// </summary>
-	public User(): this(Guid.Empty, string.Empty) {}
+	public User(): this(Guid.Empty, string.Empty) {} // required for admin panel
 }
--- Program.cs
+++ Program.cs
@@ -6,14 +6,19 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=app.db"));
+
+if (builder.Configuration["UseSqlite"] == "true")
+	builder.Services.AddDbContext<AppDbContext>(opts => opts.UseSqlite("Data Source=app.db"));
+else
+	builder.Services.AddDbContext<AppDbContext>(opts => opts.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddCoreAdmin(); // this line must come AFTER DbContext registration
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
